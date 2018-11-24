27fab05fb6ba559cc7f418afb8e57a5ab592cba7
Mbugua <embugua2009@gmail.com>
2018-11-24 18:16:06 +0300

    Renaming projects

---
+++ Hunter..CSharp.Connector/LogPayload.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunter.LogAggregator
+{
+    public enum LogConstants
+    {
+        Info,
+        Error,
+        Warning
+    }
+    public class LogPayload
+    {
+        public string Category { get; set; }
+        public string Subcategory { get; set; }
+        public LogConstants LogCategorization { get; set; }
+        public DateTime LoggingDate { get; set; }
+        public string LogMessage { get; set; }        
+        
+    }
+}
--- Hunter.UI/Startup1.cs
+++ Hunter.UI/Startup1.cs
@@ -11,7 +11,7 @@ namespace Hunter.UI
     {
         public void Configuration(IAppBuilder app)
         {
-           
+            app.MapSignalR();
         }
     }
 }
