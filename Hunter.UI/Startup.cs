84f6d3f50e0145c8a6365e10f505d96a4643c6d7
Mbugua <embugua2009@gmail.com>
2018-11-24 18:17:18 +0300

    Fixing startup

--- Hunter.UI/Startup.cs
+++ Hunter.UI/Startup.cs
@@ -3,11 +3,11 @@ using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartup(typeof(Hunter.UI.Startup1))]
+[assembly: OwinStartup(typeof(Hunter.UI.Startup))]
 
 namespace Hunter.UI
 {
-    public class Startup1
+    public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
